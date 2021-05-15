ce96240f4fb0ea167997acae06a2e1dca9de3a5b
xiaohupao <wk1996@icloud.com>
2021-05-15 21:50:22 +0800

    LeetCode01

---
+++ src/main/java/cn/xiaohupao/leetcode01/TwoSum.java
@@ -0,0 +1,51 @@
+package cn.xiaohupao.leetcode01;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author: xiaohupao
+ * @Date: 2021/5/15 19:56
+ */
+public class TwoSum{
+
+    /**
+     * Given an array of integers nums and an integer target,
+     * return indices of the two numbers such that they add up to target.
+     *
+     * Input: nums = [2,7,11,15], target = 9
+     * Output: [0,1]
+     * Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+     * @param nums Array
+     * @param target target value
+     * @return index
+     *
+     * time：O(n)
+     * space：O(n)
+     */
+    public static int[] twoSum(int[] nums, int target){
+        if (nums == null || nums.length < 2){
+            return new int[]{-1, -1};
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] res = new int[]{-1, -1};
+        for (int i = 0; i < nums.length; i++){
+            if (map.containsKey(target - nums[i])){
+                res[0] = map.get(target - nums[i]);
+                res[1] = i;
+                break;
+            }
+            map.put(nums[i], i);
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{2,7,11,15};
+        int target = 9;
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+    }
+}
--- src/main/java/cn/xiaohupao/leetcode01/TwoSunm.java
+++
@@ -1,8 +0,0 @@
-package cn.xiaohupao.leetcode01;
-
-/**
- * @Author: xiaohupao
- * @Date: 2021/5/15 19:56
- */
-public class TwoSunm {
-}
