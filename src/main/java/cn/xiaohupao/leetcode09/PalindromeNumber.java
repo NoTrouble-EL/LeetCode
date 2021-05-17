161c2eb52054128a6af76a408b226d7ec8b214a9
xiaohupao <wk1996@icloud.com>
2021-05-17 19:15:41 +0800

    LeetCode09

--- src/main/java/cn/xiaohupao/leetcode09/PalindromeNumber.java
+++ src/main/java/cn/xiaohupao/leetcode09/PalindromeNumber.java
@@ -9,7 +9,7 @@ public class PalindromeNumber {
      * Given an integer x, return true if x is palindrome integer.
      * An integer is a palindrome when it reads the same backward as forward.
      * For example, 121 is palindrome while 123 is not.
-     * @param x int
+     * @param x int - Given an integer
      * @return boolean - true if x is palindrome integer
      *
      * time: O(log(n))
