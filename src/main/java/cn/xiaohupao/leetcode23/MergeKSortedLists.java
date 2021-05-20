302e500ae287c0ad89ec9b98738922cfe8510798
xiaohupao <wk1996@icloud.com>
2021-05-20 15:33:38 +0800

    LeetCode23

--- src/main/java/cn/xiaohupao/leetcode23/MergeKSortedLists.java
+++ src/main/java/cn/xiaohupao/leetcode23/MergeKSortedLists.java
@@ -20,9 +20,13 @@ public class MergeKSortedLists {
     }
 
     /**
+     *You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+     * Merge all the linked-lists into one sorted linked-list and return it.
+     * @param lists linked-lists
+     * @return Merge all the linked-lists into one sorted linked-list
      *
-     * @param lists
-     * @return
+     * time: O(nlogk)
+     * space: O(n)
      */
     public static ListNode mergeKLists(ListNode[] lists){
         if (lists == null || lists.length == 0){
